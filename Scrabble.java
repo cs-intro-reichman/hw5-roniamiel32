5975422685979adf2f70981ebaa66b4047aa1787
Roni <roni.amiel@post.runi.ac.il>
2024-12-08 22:31:26 +0200

    Completed the mission

--- Scrabble.java
+++ Scrabble.java
@@ -103,7 +103,7 @@ public class Scrabble {
 				break;
 			} else if (!MyString.subsetOf(input, hand)) {
 				System.out.println("Invalid word. Try again.");
-			} else if (!isWordInDictionary(input)) {
+			} else if (!isWordInDictionary(input)){
 				System.out.println("No such word in the dictionary. Try again.");
 			}  else {
 					score += wordScore(input);
